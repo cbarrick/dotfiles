bb2c1933138162f9a9816ec8cc96364b70d27706
Chris Barrick <cbarrick1@gmail.com>
2017-12-07 15:06:06 -0500

    Use safer default imports in ipython

--- home/.ipython/profile_default/startup/10-imports.py
+++ home/.ipython/profile_default/startup/10-imports.py
@@ -2,9 +2,6 @@ from copy import copy, deepcopy
 from importlib import reload
 from pathlib import Path
 
-import matplotlib as mpl
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import scipy as sp
-import xarray as xr
