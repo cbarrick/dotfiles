90e5127c88063a97ea0cd8501cbe40553a002d38
Chris Barrick <cbarrick1@gmail.com>
2018-04-30 21:31:16 -0400

    Add iterm2 utilities

--- install.zsh
+++ install.zsh
@@ -9,6 +9,6 @@ done
 # Tell iTerm2 about its profile
 if [[ `uname` == 'Darwin' ]]
 then
-	defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.iterm"
+	defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.iterm2"
 	defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
 fi
