696687901f35523f86196826db2ebf453e1bb9b7
Chris Barrick <cbarrick1@gmail.com>
2020-06-06 18:52:59 -0400

    Remove conda from roronoa

--- home/.zsh/env/roronoa.zsh
+++ home/.zsh/env/roronoa.zsh
@@ -4,9 +4,3 @@ then
 	source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc
 	source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
 fi
-
-if [[ -o interactive ]]
-then
-	# Conda
-	source /usr/local/miniconda3/etc/profile.d/conda.sh
-fi
