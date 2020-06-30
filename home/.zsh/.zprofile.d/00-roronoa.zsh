a804a0d13915d971cf70a360e568be04b9c19bbd
Chris Barrick <cbarrick1@gmail.com>
2020-06-30 17:42:08 -0400

    Allow GNU core utilities to replace the defaults

--- home/.zsh/.zprofile.d/00-roronoa.zsh
+++ home/.zsh/.zprofile.d/00-roronoa.zsh
@@ -1,5 +1,12 @@
 if [[ "$HOST" == "roronoa" ]]
 then
+	# Allow GNU core utilities to replace the defaults.
+	path=(
+		'/usr/local/opt/coreutils/libexec/gnubin'
+		'/usr/local/opt/gnu-sed/libexec/gnubin'
+		$path
+	)
+
 	# Enable Google Cloud SDK.
 	source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc
 	source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
