f6ead77f1540f9a2bdbc20dbf22d8b7130d34b80
Chris Barrick <cbarrick1@gmail.com>
2021-03-03 13:44:51 -0500

    Remove gcp setup on roronoa

--- home/.zsh/.zprofile.d/00-roronoa.zsh
+++ home/.zsh/.zprofile.d/00-roronoa.zsh
@@ -6,8 +6,4 @@ then
 		'/usr/local/opt/gnu-sed/libexec/gnubin'
 		$path
 	)
-
-	# Enable Google Cloud SDK.
-	source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc
-	source /usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc
 fi
