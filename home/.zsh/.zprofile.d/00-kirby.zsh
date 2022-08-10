65efbf592000a8a0741c48e1088968a0c49a7ace
Chris Barrick <cbarrick1@gmail.com>
2022-08-10 19:29:33 -0400

    Fix CUDA on kirby

--- home/.zsh/.zprofile.d/00-kirby.zsh
+++ home/.zsh/.zprofile.d/00-kirby.zsh
@@ -1,10 +1,11 @@
 if [[ "${HOST%%.*}" == "kirby" ]]
 then
+	# CUDA Setup
+	# Nvidia ships several binaries that conflict with the system
+	# toolchain, e.g. gcc. Therefore, CUDA must be added to the
+	# *end* of the path.
+	export CUDA_PATH="/opt/cuda"
 	export CPPFLAGS="-I/opt/cuda/include"
 	export LDLIBS="-L/opt/cuda/lib64"
-
-	path=(
-		"/opt/cuda/bin"
-		${path}
-	)
+	path=(${path} "/opt/cuda/bin")
 fi
