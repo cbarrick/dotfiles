84f3536b4c1ce5575ae470d7e35c9292b91ba362
Chris Barrick <cbarrick1@gmail.com>
2023-02-04 16:36:51 -0500

    Add Google Cloud SDK to the PATH on samus

--- home/.zsh/.zprofile.d/00-samus.zsh
+++ home/.zsh/.zprofile.d/00-samus.zsh
@@ -55,5 +55,6 @@ then
 	)
 
 	source "${HOMEBREW_PREFIX}/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+	source "${HOMEBREW_PREFIX}/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
 	source "${HOME}/.poetry/env"
 fi
