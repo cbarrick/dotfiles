35e636913c48bd1c5f32a8f0d244e02a5680d7f6
Chris Barrick <cbarrick1@gmail.com>
2026-06-13 16:25:58 -0400

    Add GNU tar to path on samus

--- home/.zsh/.zprofile.d/00-samus.zsh
+++ home/.zsh/.zprofile.d/00-samus.zsh
@@ -21,6 +21,7 @@ then
 		"${HOMEBREW_PREFIX}/opt/llvm/bin"
 		"${HOMEBREW_PREFIX}/opt/coreutils/libexec/gnubin"
 		"${HOMEBREW_PREFIX}/opt/gnu-sed/libexec/gnubin"
+		"${HOMEBREW_PREFIX}/opt/gnu-tar/libexec/gnubin"
 		"${HOMEBREW_PREFIX}/opt/make/libexec/gnubin"
 		"${HOMEBREW_PREFIX}/opt/ruby/bin"
 		"${HOMEBREW_PREFIX}/bin"
@@ -42,6 +43,7 @@ then
 		"${HOMEBREW_PREFIX}/opt/llvm/share/man"
 		"${HOMEBREW_PREFIX}/opt/coreutils/libexec/gnuman"
 		"${HOMEBREW_PREFIX}/opt/gnu-sed/libexec/gnuman"
+		"${HOMEBREW_PREFIX}/opt/gnu-tar/libexec/gnuman"
 		"${HOMEBREW_PREFIX}/opt/make/libexec/gnuman"
 		"${HOMEBREW_PREFIX}/opt/ruby/share/man"
 		"${HOMEBREW_PREFIX}/share/man"
@@ -53,6 +55,7 @@ then
 		"${HOME}/.local/share/info"
 		"${HOMEBREW_PREFIX}/opt/coreutils/libexec/gnuinfo"
 		"${HOMEBREW_PREFIX}/opt/gnu-sed/libexec/gnuinfo"
+		"${HOMEBREW_PREFIX}/opt/gnu-tar/libexec/gnuinfo"
 		"${HOMEBREW_PREFIX}/opt/make/libexec/gnuinfo"
 		"${HOMEBREW_PREFIX}/share/info"
 		${infopath}
